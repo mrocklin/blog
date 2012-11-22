314dcc322c19fc12d8dbe3684848ca375c6dd456
Matthew Rocklin <mrocklin@gmail.com>
2012-11-22 08:23:02 -0600

    add scripts directory

---
+++ scripts/2012-11-23-BuildingComputations.py
@@ -0,0 +1,21 @@
+from sympy.matrices.expressions import MatrixSymbol
+from sympy import Q, Symbol
+from sympy.computations.inplace import inplace_compile
+from sympy.computations.core import Identity
+from sympy.computations.matrices.compile import make_rule, patterns
+
+a,b,c,n = map(Symbol, 'abcn')
+X = MatrixSymbol('X', n, n)
+Y = MatrixSymbol('Y', n, n)
+Z = MatrixSymbol('Z', n, n)
+W = MatrixSymbol('W', n, n)
+expr = (Y.I*Z*Y + b*Z*Y + c*W*W).I*Z*W
+comp = Identity(expr)
+assumptions = Q.symmetric(Y) & Q.positive_definite(Y) & Q.symmetric(X)
+rule = make_rule(patterns, assumptions)
+mathcomp = next(rule(comp))
+mathcomp.show()
+icomp = inplace_compile(mathcomp)
+icomp.show()
+
+
